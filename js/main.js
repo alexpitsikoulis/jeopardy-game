7c676f1b59b4e700f361c4d5d53d0bae785a28ad
Alex Pitsikoulis <alex.pitsikoulis@gmail.com>
2019-06-12 12:26:09 -0400

    start button listener now populates player name and score in the footer from the info in the player object

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ const player = {
 //event listener on start button that saves player name hides welcome screen and shows game board
 $('#start-button').on('click', function() {
     player.name = $('#player-name-input').val()
+    $('#player-name-display').html(player.name)
+    $('#player-score-display').html(player.score)
     $('.welcome-screen').fadeOut(100)
     $('.roundOneGameBoard').fadeIn(100)
 })
